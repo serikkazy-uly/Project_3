d0c9efbe35ee0cddea1c3c2a86ad09e362efc7c6
serikkazy-uly <damirkap89@gmail.com>
2024-01-21 14:07:34 +0600

    include class by composer

---
+++ app/QueryBuilder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+// use Aura\SqlQuery\QueryFactory;
+
+
+class QueryBuilder{
+    public function getAll($table){
+        // $queryFactory = new QueryFactory('mysql');
+        // $select = $queryFactory->newSelect();
+        // $select->cols(['*'])
+        // ->from('posts');
+
+        // $pdo = new PDO("mysql:host=mysql; dbname=laravel;charset=utf8;", "user", "secret");
+        // $sth=$pdo->prepare($select->getStatement());
+        // $sth->execute($select->getBindValues());
+        // $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
--- public/index.php
+++ public/index.php
@@ -4,25 +4,7 @@
 require '../vendor/autoload.php';
 
 // use namespace
-use Aura\SqlQuery\QueryFactory;
+use App\QueryBuilder;
+$db = new QueryBuilder();
 
-$queryFactory = new QueryFactory('mysql'); //init
-$select = $queryFactory->newSelect();
-
-$select->cols(['*'])
-    ->from('posts');
-
-    // var_dump($select->getStatement());die;
-    // a PDO connection
-$pdo = new PDO("mysql:host=mysql; dbname=laravel; charset=utf8;", "user", "secret");
-
-// prepare the statement
-$sth = $pdo->prepare($select->getStatement());
-
-// bind the values and execute
-$sth->execute($select->getBindValues());
-
-// get the results back as an associative array
-$result = $sth->fetchAll(PDO::FETCH_ASSOC);
-
-var_dump($result);
+var_dump($db);
