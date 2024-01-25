6a9f950afdab6b26e39c5c60888b11756b678a55
serikkazy-uly <damirkap89@gmail.com>
2024-01-25 10:49:45 +0600

    add kint & flash

--- public/index.php
+++ public/index.php
@@ -1,11 +1,34 @@
 <?php
-
+if( !session_id() ) {
+    session_start();
+}
+    
 require '../vendor/autoload.php';
+use function Tamtamchik\SimpleFlash\flash;
 
-// Create new Plates instance
-$templates = new League\Plates\Engine('../app/views');
-// var_dump($templates);die;
+if (true) {
+    flash()->message('Hot!');
+}
 
-// Render a template
-// echo $templates->render('homepage', ['name' => 'Jonathan']);
-echo $templates->render('about', ['title' => 'Jonathan']);
+echo flash()->display();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    
+
+
+
+</body>
+
+</html>
