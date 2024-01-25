ff5d7d2a0f106048ff61e5f6cb502a265eee510e
serikkazy-uly <damirkap89@gmail.com>
2024-01-25 11:45:55 +0600

    add Routing by func

--- public/index.php
+++ public/index.php
@@ -4,31 +4,44 @@ if( !session_id() ) {
 }
     
 require '../vendor/autoload.php';
-use function Tamtamchik\SimpleFlash\flash;
+// use function Tamtamchik\SimpleFlash\flash;
+// d($_SERVER);die;
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/users', 'get_all_users_handler');
+    // {id} must be a number (\d+)
+    $r->addRoute('GET', '/user/{id:\d+}', 'get_user_handler');
+    // The /{title} suffix is optional
+    $r->addRoute('GET', '/articles/{id:\d+}[/{title}]', 'get_article_handler');
+});
 
-if (true) {
-    flash()->message('Hot!');
+// Fetch method and URI from somewhere
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND: //0
+        // ... 404 Not Found
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED: // 2
+        $allowedMethods = $routeInfo[1];
+        // ... 405 Method Not Allowed
+        break;
+    case FastRoute\Dispatcher::FOUND: // 1
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        // d($handler, $vars);
+        call_user_func($handler,$vars);
+        // ... call $handler with $vars
+        break;
 }
 
-echo flash()->display();
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-</head>
-
-<body>
-    
-
-
-
-</body>
-
-</html>
+function get_user_handler($vars){
+    d($vars['id']);
+}
