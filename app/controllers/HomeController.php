e7113038173587172e9d417c00601ee1f4c64b77
serikkazy-uly <damirkap89@gmail.com>
2024-01-27 10:46:22 +0600

    add role

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -26,8 +26,18 @@ class HomeController
     }
     public function index($vars)
     {
+        // var_dump($this->auth->admin()->addRoleForUserById(1, \Delight\Auth\Role::ADMIN));
+        d($this->auth->getRoles());
+        die;
+        // d($this->auth->isLoggedIn());die; 
+        // $this->auth->login('dana@gmail.com', '123');die;
+        try {
+            $this->auth->admin()->addRoleForUserById(1, \Delight\Auth\Role::ADMIN);
+        } catch (\Delight\Auth\UnknownIdException $e) {
+            die('Unknown user ID');
+        }
+        die;
 
-        d($this->auth->getEmail());die;
         $db = new QueryBuilder();
         $posts = $db->getAll('posts');
         // Render a template
@@ -43,22 +53,18 @@ class HomeController
                 echo '  For emails, consider using the mail(...) function, Symfony Mailer, Swiftmailer, PHPMailer, etc.';
                 echo '  For SMS, consider using a third-party service and a compatible SDK';
             });
-        
+
             echo 'We have signed up a new user with the ID ' . $userId;
-        }
-        catch (\Delight\Auth\InvalidEmailException $e) {
+        } catch (\Delight\Auth\InvalidEmailException $e) {
             die('Invalid email address');
-        }
-        catch (\Delight\Auth\InvalidPasswordException $e) {
+        } catch (\Delight\Auth\InvalidPasswordException $e) {
             die('Invalid password');
-        }
-        catch (\Delight\Auth\UserAlreadyExistsException $e) {
+        } catch (\Delight\Auth\UserAlreadyExistsException $e) {
             die('User already exists');
-        }
-        catch (\Delight\Auth\TooManyRequestsException $e) {
+        } catch (\Delight\Auth\TooManyRequestsException $e) {
             die('Too many requests');
         }
-        
+
         // echo $this->templates->render('user', ['name' => 'Page user Jonathan', 'flash' => $this->flash]);
     }
 
@@ -68,42 +74,33 @@ class HomeController
     {
         try {
             $this->auth->confirmEmail('Lc6isy6HG1F4Vi34', 'YoCN5sAVfqBP1g6J');
-        
+
             echo 'Email address has been verified';
-        }
-        catch (\Delight\Auth\InvalidSelectorTokenPairException $e) {
+        } catch (\Delight\Auth\InvalidSelectorTokenPairException $e) {
             die('Invalid token');
-        }
-        catch (\Delight\Auth\TokenExpiredException $e) {
+        } catch (\Delight\Auth\TokenExpiredException $e) {
             die('Token expired');
-        }
-        catch (\Delight\Auth\UserAlreadyExistsException $e) {
+        } catch (\Delight\Auth\UserAlreadyExistsException $e) {
             die('Email address already exists');
-        }
-        catch (\Delight\Auth\TooManyRequestsException $e) {
+        } catch (\Delight\Auth\TooManyRequestsException $e) {
             die('Too many requests');
         }
     }
 
-public function login(){
-    try {
-        $this->auth->login('dana@gmail.com', '123');
-    
-        echo 'User is logged in';
-    }
-    catch (\Delight\Auth\InvalidEmailException $e) {
-        die('Wrong email address');
-    }
-    catch (\Delight\Auth\InvalidPasswordException $e) {
-        die('Wrong password');
-    }
-    catch (\Delight\Auth\EmailNotVerifiedException $e) {
-        die('Email not verified');
-    }
-    catch (\Delight\Auth\TooManyRequestsException $e) {
-        die('Too many requests');
-    }
-}
+    public function login()
+    {
+        try {
+            $this->auth->login('dana@gmail.com', '123');
 
-
+            echo 'User is logged in';
+        } catch (\Delight\Auth\InvalidEmailException $e) {
+            die('Wrong email address');
+        } catch (\Delight\Auth\InvalidPasswordException $e) {
+            die('Wrong password');
+        } catch (\Delight\Auth\EmailNotVerifiedException $e) {
+            die('Email not verified');
+        } catch (\Delight\Auth\TooManyRequestsException $e) {
+            die('Too many requests');
+        }
+    }
 }
